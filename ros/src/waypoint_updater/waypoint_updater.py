4307a4e72d918e3e51edbcf3877a1146039dfdfe
Will Roe <r.will.roe@gmail.com>
2018-11-18 20:08:18 -0800

    fix typo

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -65,7 +65,7 @@ class WaypointUpdater(object):
         val = np.dot(cl_vect-prev_vect, pos_vect - cl_vect)
 
         if val > 0:
-            closest_idx = (closest_idx + 1) % len(self.waypoints_2d))
+            closest_idx = (closest_idx + 1) % len(self.waypoints_2d)
         return closest_idx
 
     def publish_waypoint(self, closest_idx):

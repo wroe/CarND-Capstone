cb1d2878604bfe3614df8ff31d3db71f9abb467b
Will Roe <r.will.roe@gmail.com>
2018-11-21 19:43:17 -0800

    last typo. waypoint updater now produces /final_waypoints (zeros)

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -83,7 +83,7 @@ class WaypointUpdater(object):
         # Recieve and store waypoints
         self.base_waypoints = waypoints
         if not self.waypoints_2d:
-            self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints]
+            self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
             self.waypoint_tree = KDTree(self.waypoints_2d)
 
     def traffic_cb(self, msg):

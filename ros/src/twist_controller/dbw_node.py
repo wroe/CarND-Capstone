807cc45e19a98298aa6394dab2533462e391c368
Will Roe <r.will.roe@gmail.com>
2018-11-25 01:40:32 -0800

    fix indentation

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -89,8 +89,8 @@ class DBWNode(object):
                                                                  self.angular_vel)
 
 
-           if self.dbw_enabled:
-               self.publish(self.throttle, self.brake, self.steer)
+        if self.dbw_enabled:
+            self.publish(self.throttle, self.brake, self.steer)
             rate.sleep()
 
     def dbw_enabled_cb(self, msg):

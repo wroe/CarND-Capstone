14bec7e17bd55ce8194510abd2528849e2223371
Will Roe <r.will.roe@gmail.com>
2018-11-24 19:29:44 -0800

    twist controller walkthrough

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -54,14 +54,15 @@ class DBWNode(object):
                                          BrakeCmd, queue_size=1)
 
         # TODO: Create `Controller` object
-        self.controller = Controller(   vehicle_mass=vehicle_mass,
-                                        brake_deadband=brake_deadband,
-                                        decel_limit=decel_limit,
-                                        accel_limit=accel_limit,
-                                        wheel_radius=wheel_radius,
-                                        steer_ratio=steer_ratio,
-                                        max_lat_accel=max_lat_accel,
-                                        max_steer_angle=max_steer_angle)
+        self.controller = Controller(   vehicle_mass = vehicle_mass,
+                                        brake_deadband = brake_deadband,
+                                        decel_limit = decel_limit,
+                                        accel_limit = accel_limit,
+                                        wheel_radius = wheel_radius,
+                                        wheel_base = wheel_base
+                                        steer_ratio = steer_ratio,
+                                        max_lat_accel = max_lat_accel,
+                                        max_steer_angle = max_steer_angle)
 
         # TODO: Subscribe to all the topics you need to
         rospy.Subscriber('/vehicle/dbw_enabled', Bool, self.dbw_enabled_cb)

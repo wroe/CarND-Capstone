69f48d3fec089494ade72798a63524016837f2b6
Will Roe <r.will.roe@gmail.com>
2018-11-26 18:25:15 -0800

    fix mistake in dbw node loop

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -84,7 +84,7 @@ class DBWNode(object):
         while not rospy.is_shutdown():
             # TODO: Get predicted throttle, brake, and steering using `twist_controller`
             # You should only publish the control commands if dbw is enabled
-            if not None in (self.throttle, self.brake, self.steering):
+            if not None in (self.current_vel, self.linear_vel, self.angular_vel, self.dbw_enabled):
                 self.throttle, self.brake, self.steering = self.controller.control(self.current_vel,
                                                                  self.dbw_enabled,
                                                                  self.linear_vel,

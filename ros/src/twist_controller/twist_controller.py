2fe3421104221ed46762da149b184aca178b902b
wroe <r.will.roe@gmail.com>
2018-12-07 03:49:38 +0000

    bugfix for older twist_controller code

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -28,7 +28,7 @@ class Controller(object):
         self.vehicle_mass = vehicle_mass
         self.fuel_capacity = fuel_capacity
         self.brake_deadband = brake_deadband
-        self.deccel_limit = decel_limit
+        self.decel_limit = decel_limit
         self.accel_limit = accel_limit
         self.wheel_radius = wheel_radius
 

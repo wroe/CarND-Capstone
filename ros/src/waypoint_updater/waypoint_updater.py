adb86aea421752ee2a4e6ceaab886651b6b4fdb1
wroe <r.will.roe@gmail.com>
2018-12-17 14:38:13 +0000

    bugfix traffic_cb msg.data, car stops at red lights!

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -125,7 +125,7 @@ class WaypointUpdater(object):
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
-        self.stopline_wp_idx = msg
+        self.stopline_wp_idx = msg.data
 
     def obstacle_cb(self, msg):
         # TODO: Callback for /obstacle_waypoint message. We will implement it later

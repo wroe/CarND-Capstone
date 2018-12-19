a701b8460bc9c304f41851706f5ce28af0fdf6ba
Will Roe <r.will.roe@gmail.com>
2018-12-18 20:50:26 -0700

    skeleton code to use classifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,17 @@
 from styx_msgs.msg import TrafficLight
+import rospy
+from keras.models import load_model
+
+IMG_H = 600   # image height in pixels
+IMG_W = 800  # image width in pixels
+IMG_C = 3     # num of channels
 
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
-        pass
+        self.model_dir_path =  './models/sim_model.h5'
+        self.model = load_model(self.model_dir_path)
+        self.model._make_predict_function()
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -16,4 +24,8 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        colors = [TrafficLight.RED, TrafficLight.GREEN, TrafficLight.UNKNOWN]
+        img = np.reshape (image,  (1, IMG_H, IMG_W, IMG_C))
+        colorNum = np.argmax(self.model.predict(img))
+        
+        return colors[colorNum] # -1 is UNKNOWN
